82033f1b7441bc1239d0db80369dbcfd10499b76
Olav Stornes <olav.d.stornes@gmail.com>
2021-02-26 22:42:19 +0100

    Extract modpack to a single file

--- main.py
+++ main.py
@@ -1,46 +1,9 @@
 from pathlib import Path
+import modpack
 
 INPUT_FOLDER = 'input'
 ROOT_FOLDER = 'root_folder'
 OUTPUT_FOLDER = 'output'
-
-
-class ModPack():
-    def __init__(self, mod_folder):
-        self.modname = mod_folder.name
-        self.mod_folder = mod_folder
-        self.mod_root_folder = mod_folder / ROOT_FOLDER
-        self.out_p = Path(OUTPUT_FOLDER) / ROOT_FOLDER
-        self.add_mod()
-
-    def convert_from_input_to_output(self, in_path: Path):
-        abs_input = str(in_path.resolve()) 
-        x = abs_input.replace(str(self.mod_root_folder.resolve()), '').lstrip('/')
-        output = self.out_p.joinpath(x)
-        return output
-
-    def handle_symlinking(self, file_path: Path):
-        target_path = self.convert_from_input_to_output(file_path)
-        if target_path.exists():
-            print("DELETE {}".format(target_path))
-            target_path.unlink()
-        print("{} --> {}".format(file_path.resolve(), target_path.resolve()))
-        file_path.link_to(target_path)
-
-    def create_folder(self, folder_path: Path):
-        print(folder_path)
-        output_path = self.convert_from_input_to_output(folder_path)
-        print("FOLDER AT {}".format(output_path.resolve()))
-        output_path.mkdir(exist_ok=True)
-
-
-
-    def add_mod(self):
-        for input_path in self.mod_folder.glob('**/*'):
-            if input_path.is_dir():
-                self.create_folder(input_path)
-                continue
-            self.handle_symlinking(input_path)
 
 
 if __name__ == "__main__":
@@ -48,4 +11,4 @@ if __name__ == "__main__":
 
     for single_mod in sorted(in_p.iterdir()):
         print("~~~~ Adding {}".format(single_mod.name))
-        ModPack(single_mod)
+        modpack.ModPack(single_mod)
