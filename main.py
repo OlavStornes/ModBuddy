0679b245b1388bdd509a72544e5bd17d1b4c90f1
Olav Stornes <olav.d.stornes@gmail.com>
2021-02-26 23:04:45 +0100

    Rename settings-file

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import json
 import modpack
 
 INPUT_FOLDER = 'input'
+SETTINGS_NAME = 'settings.json'
 ROOT_FOLDER_NAME = 'root_folder'
 OUTPUT_FOLDER = Path('output') / ROOT_FOLDER_NAME
 
 
 if __name__ == "__main__":
     in_p = Path(INPUT_FOLDER)
-    settings_path = in_p / 'settings.json'
-    test = json.loads(settings_path.read_text())
-    print(test)
-    for single_mod in test:
+    settings = json.loads((in_p / SETTINGS_NAME).read_text())
+    print(settings)
+    for single_mod in settings:
         print("~~~~ Adding {}".format(single_mod.get('name')))
         target_folder = in_p / single_mod.get('folder_name')
         modpack.ModPack(target_folder, OUTPUT_FOLDER)
