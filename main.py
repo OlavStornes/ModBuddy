f7727048aa195fd4749bbff2ab144b6bf4bca82f
Olav Stornes <olav.d.stornes@gmail.com>
2021-02-26 22:57:34 +0100

    Add json to the mix as a setting

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pathlib import Path
+import json
 import modpack
 
 INPUT_FOLDER = 'input'
-ROOT_FOLDER = 'root_folder'
-OUTPUT_FOLDER = 'output'
+ROOT_FOLDER_NAME = 'root_folder'
+OUTPUT_FOLDER = Path('output') / ROOT_FOLDER_NAME
 
 
 if __name__ == "__main__":
     in_p = Path(INPUT_FOLDER)
-
-    for single_mod in sorted(in_p.iterdir()):
-        print("~~~~ Adding {}".format(single_mod.name))
-        modpack.ModPack(single_mod)
+    settings_path = in_p / 'settings.json'
+    test = json.loads(settings_path.read_text())
+    print(test)
+    for single_mod in test:
+        print("~~~~ Adding {}".format(single_mod.get('name')))
+        target_folder = in_p / single_mod.get('folder_name')
+        modpack.ModPack(target_folder, OUTPUT_FOLDER)
